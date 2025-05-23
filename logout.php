eec0c7da6f6a2ddf02a09060fbc1d5be2150a6a0
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-05-23 21:59:48 +0800

    change the background to the smaller image from a licensed source

--- logout.php
+++ logout.php
@@ -10,7 +10,7 @@
         body {
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
             margin: 0;
-            background-image: url('file/greenwallpaper.jpeg');
+            background-image: url('file/greentexture.jpg');
         }
     </style>
     <meta http-equiv='refresh' content='2; url=login.php'>

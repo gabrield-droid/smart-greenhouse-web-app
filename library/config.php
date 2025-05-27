0258051db23d3577fa38bed4f4e410fcc021502d
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-05-27 23:27:56 +0800

    separate the mysql database credentials into another file

--- library/config.php
+++ library/config.php
@@ -1,8 +1,10 @@
 <?php
+    require __DIR__."/db_config.php";
+    
     $host = "localhost";
-    $user = "admin";
-    $pass = "admin";
-    $db = "smart_greenhouse";
+    $user = DB_USER;
+    $pass = DB_USER;
+    $db = DB_NAME;
 
     $con = mysqli_connect($host, $user, $pass, $db);
     if (mysqli_connect_errno()) {

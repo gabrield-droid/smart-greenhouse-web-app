f9cd3ac7921543ce21bd15c980fc18c9b8c46b42
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-06-09 00:38:53 +0800

    make every hypelink open in a new tab

--- contents/about.php
+++ contents/about.php
@@ -20,13 +20,13 @@
         </div>
         <div class="about-item">
             <h3>Pengakuan/Atribusi</h3>
-            <div>Semua ikon dibuat oleh pengguna bernama <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> di <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-            <div>Latar belakang tekstur hijau oleh pengguna bernama <a href="https://unsplash.com/@enginakyurt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">engin akyurt</a> di <a href="https://unsplash.com/photos/green-textile-in-close-up-image-HEMIBJ8QQuA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></div>
+            <div>Semua ikon dibuat oleh pengguna bernama <a href="https://www.flaticon.com/authors/smashicons" target="_blank" title="Smashicons">Smashicons</a> di <a href="https://www.flaticon.com/" target="_blank" title="Flaticon">www.flaticon.com</a></div>
+            <div>Latar belakang tekstur hijau oleh pengguna bernama <a href="https://unsplash.com/@enginakyurt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">engin akyurt</a> di <a href="https://unsplash.com/photos/green-textile-in-close-up-image-HEMIBJ8QQuA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a></div>
         </div>
         <div class="about-item">
             <h3>Lisensi</h3>
-            <div>Aplikasi ini dilisensikan di bawah <strong><a href="https://www.gnu.org/licenses/agpl-3.0.html">GNU Affero General Public License versi 3.0 (AGPL-3.0)</a></strong>. Anda berhak <strong>memperoleh</strong>, <strong>mempelajari</strong>, serta <strong>memodifikasi</strong> kode sumbernya dengan ketentuan bahwa hasil modifikasinya juga harus dilisensikan di bawah <strong>GNU Affero General Public License versi 3.0 (AGPL-3.0)</strong>.</div>
-            <div>Repositori kode sumber dapat diakses di: <a href="https://github.com/gabrield-droid/smart-greenhouse-web-app.git">https://github.com/gabrield-droid/smart-greenhouse-web-app.git</a></div>
+            <div>Aplikasi ini dilisensikan di bawah <strong><a href="https://www.gnu.org/licenses/agpl-3.0.html" target="_blank">GNU Affero General Public License versi 3.0 (AGPL-3.0)</a></strong>. Anda berhak <strong>memperoleh</strong>, <strong>mempelajari</strong>, serta <strong>memodifikasi</strong> kode sumbernya dengan ketentuan bahwa hasil modifikasinya juga harus dilisensikan di bawah <strong>GNU Affero General Public License versi 3.0 (AGPL-3.0)</strong>.</div>
+            <div>Repositori kode sumber dapat diakses di: <a href="https://github.com/gabrield-droid/smart-greenhouse-web-app.git" target="_blank">https://github.com/gabrield-droid/smart-greenhouse-web-app.git</a></div>
         </div>
     </div>
 </div>

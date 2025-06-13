77c1f2266ef12f2680a0531f847d154a612b0831
Gabriel Dylan <gabrieldylan474@gmail.com>
2025-06-13 20:43:00 +0800

    logging out will now immediately redirect to the login page

--- logout.php
+++ logout.php
@@ -1,21 +1,6 @@
 <?php
     session_start();
     session_destroy();
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <style type="text/css">
-        body {
-            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-            margin: 0;
-            background-image: url('files/greentexture.jpg');
-        }
-    </style>
-    <meta http-equiv='refresh' content='2; url=login.php'>
-</head>
-<body>
-    <p align="center" style="color: white">Anda telah keluar!</p>
-</body>
-</html>
+    header("location: login.php");
+?>
